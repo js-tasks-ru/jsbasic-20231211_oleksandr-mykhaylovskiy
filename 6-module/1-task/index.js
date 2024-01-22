ec76a8dfa5b0907d0701dea855f09d9525c3ca30
Oleksandr Mykhailovskiy <aleksejmikhajlovs@gmail.com>
2024-01-22 13:51:32 +0200

    feat: some issues with tests complete

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,65 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.#createTable();
+  }
+
+  #createTable(){
+    const table = document.createElement("table");
+    table.append(this.#createTableHeading());
+
+    for(let {name, age, salary, city} of this.rows){
+      const tableRow = this.#createTableRow({name, age, salary, city});
+      table.append(tableRow);
+    }
+
+    table.addEventListener("click", (event) => {
+      if(event.target.innerHTML === 'X'){
+        event.target.closest('tr').remove();
+      }
+    });
+    
+    return table;
+  }
+
+  #createTableHeading(){
+    const tableHeaderRow = document.createElement("tr");
+    const thName = document.createElement("th");
+    const thAge = document.createElement("th");
+    const thSalary = document.createElement("th");
+    const thCity = document.createElement("th");
+
+    thName.innerHTML = 'Имя';
+    thAge.innerHTML = 'Возраст';
+    thSalary.innerHTML = 'Зарплата';
+    thCity.innerHTML = 'Город';
+
+    tableHeaderRow.append(thName, thAge, thSalary, thCity);
+
+    return tableHeaderRow
+  }
+
+  #createTableRow({name, age, salary, city}){
+    const tableRow = document.createElement("tr");
+    const tdName = document.createElement("td");
+    const tdAge = document.createElement("td");
+    const tdSalary = document.createElement("td");
+    const tdCity = document.createElement("td");
+    const tdCross = document.createElement("td");
+
+    tdName.innerHTML = name;
+    tdAge.innerHTML = age;
+    tdSalary.innerHTML = salary;
+    tdCity.innerHTML = city;
+    tdCross.innerHTML = '<button>X</button>';
+
+    tableRow.append(tdName, tdAge, tdSalary, tdCity, tdCross);
+
+    return tableRow
+  }
+
+  get() {
+    return this.elem;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,129 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this._container = this.#getCarousel();
+
+    document.addEventListener('DOMContentLoaded', () => {
+      this.#setArrowHide({
+        arrowRight: this._container.querySelector(".carousel__arrow_right"),
+        arrowLeft: this._container.querySelector(".carousel__arrow_left"),
+        transformValue: this.transformValue,
+        elementsCount: this.slides.length,
+        carouselInnerWidth: this._container.querySelector(".carousel__inner").offsetWidth,
+      });
+    });
+  }
+
+  transformValue = 0;
+
+  get elem(){
+    return this._container;
+  }
+
+  #getCarousel() {
+    const carousel =  createElement(`
+      <div class='carousel'>
+        <div class='carousel__arrow carousel__arrow_right'>
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class='carousel__inner'>
+          ${this.slides.map(({id, image, price, name}) => `
+            <div class="carousel__slide" data-id="${id}">
+              <img src="/assets/images/carousel/${image}" class="carousel__img" alt="slide">
+              <div class="carousel__caption">
+                <span class="carousel__price">€${price.toFixed(2)}</span>
+                <div class="carousel__title">${name}</div>
+                <button type="button" class="carousel__button">
+                  <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                </button>
+              </div>
+            </div>
+          `).join('')}
+        </div>
+        <div class='carousel__arrow carousel__arrow_left'>
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+      </div>
+    `);
+
+    this.#addArrowsEventListeners(carousel);
+    this.#addToCartEventListener(carousel);
+
+    return carousel;
+  }
+
+  #addToCartEventListener(carousel){
+    carousel.addEventListener("click", (event) => {
+      if(event.target.closest("button").classList.contains("carousel__button")){
+        const slideId = event.target.closest(".carousel__slide").dataset.id;
+    
+        const customEvent = new CustomEvent('product-add', {
+          detail: slideId,
+          bubbles: true,
+        });
+    
+        carousel.dispatchEvent(customEvent);
+
+        return;
+      }
+    })
+  }
+
+  #addArrowsEventListeners(carousel){
+    carousel.addEventListener("click", (event) => {
+      const arrowLeft = carousel.querySelector(".carousel__arrow_left");
+      const arrowRight = carousel.querySelector(".carousel__arrow_right");
+      const carouselInner = carousel.querySelector(".carousel__inner");
+  
+      const carouselInnerWidth = carouselInner.offsetWidth;
+      const elementsCount = carouselInner.childElementCount;
+
+      if(event.target.closest("div").classList.contains("carousel__arrow_right")){
+        // increase the counter for 1 slide width
+        this.transformValue += carouselInnerWidth;
+        // add the transition
+        carouselInner.style.transform = `translateX(-${this.transformValue}px)`;
+        // config the arrows visibility
+        this.#setArrowHide({
+          arrowRight: arrowRight,
+          arrowLeft: arrowLeft,
+          transformValue: this.transformValue,
+          elementsCount: elementsCount,
+          carouselInnerWidth: carouselInnerWidth,
+        });
+        return;
+      }
+      if(event.target.closest("div").classList.contains("carousel__arrow_left")){
+        // decrease the counter for 1 slide width
+        this.transformValue -= carouselInnerWidth;
+        // add the transition
+        carouselInner.style.transform = `translateX(-${this.transformValue}px)`;
+        // config the arrows visibility
+        this.#setArrowHide({
+          arrowLeft: arrowLeft,
+          arrowRight: arrowRight,
+          transformValue: this.transformValue,
+          elementsCount: elementsCount,
+          carouselInnerWidth: carouselInnerWidth,
+        })
+        return;
+      }
+    })
+  }
+
+  #setArrowHide({arrowRight, arrowLeft, transformValue, elementsCount, carouselInnerWidth}){
+    // the arrows are visible by default
+    arrowRight.style.display = 'flex';
+    arrowLeft.style.display = 'flex';
+    // if there is a first slide - left arrow is hidden, and the right one is shown
+    if(transformValue===0){
+      arrowLeft.style.display = 'none';
+      arrowRight.style.display = 'flex';
+    }
+    // if there is a last slide - left arrow is shown, and the right one is hidden
+    if(transformValue===carouselInnerWidth*(elementsCount-1)){
+      arrowRight.style.display = 'none';
+      arrowLeft.style.display = 'flex';
+    }
   }
 }
